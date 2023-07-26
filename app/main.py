714afe7098909eecaae5ef29d8f13508bd9202a4
maslukasz <lukesekmc@gmail.com>
2023-07-26 21:39:52 +0200

    app dir files push

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Path
+
+from db.database import database
+from db.dals.history_dal import HistoryDAL
+
+app = FastAPI()
+
+# @app.on_event("startup")
+# async def startup():
+#     await database.connect()
+
+@app.get("/user/{user_id}")
+async def user_all_guilds(user_id: int):
+    return await HistoryDAL().get_guild_user(user_id=user_id)
+
+@app.get("/guilds/{guild_id}/{day}")
+async def date_guild(guild_id: int, day: int=Path(gt=1, le=30)):
+    return await HistoryDAL().get_date_messages(guild_id=guild_id, day=day)
